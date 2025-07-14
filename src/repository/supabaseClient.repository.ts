968f7fa199085a023dbb77e7ba78ce0da37c218b
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-07-14 18:16:43 +0100

    Initial Setup

--- src/repository/supabaseClient.repository.ts
+++ src/repository/supabaseClient.repository.ts
@@ -1,5 +1,13 @@
 import { supabaseClient } from "../config/supabaseConfig";
-
+// primary setup will update things later
 export class supabaseRepository {
-    
+  public updateColumn = async (PAYLOAD: any) => {
+    try {
+      const { data, error } = await supabaseClient
+        .schema("kpis")
+        .from(PAYLOAD.table_name)
+        .update('')
+        .eq("client_id", PAYLOAD.client_id);
+    } catch (error) {}
+  };
 }
