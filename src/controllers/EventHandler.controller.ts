7bb0f3533a30bc8a51feac46c29758ccc5c43b8d
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-07-14 17:06:34 +0100

    Setup done

---
+++ src/controllers/EventHandler.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export class EventHandler {
+  public CatchEvent = async () => {
+    try {
+        
+    } catch (error) {
+        
+    }
+  };
+}
