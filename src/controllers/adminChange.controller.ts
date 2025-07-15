94219394ecc473cba1804c7692854f1cf117a8ca
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-07-15 17:02:36 +0100

    ✅

--- src/controllers/adminChange.controller.ts
+++ src/controllers/adminChange.controller.ts
@@ -32,7 +32,7 @@ export class adminChange {
       );
 
       return res.status(200).json({
-        message: "Client admin change event handled successfully",
+        message: "✅ Client admin change event handled successfully",
       });
     } catch (error) {
       console.error("❌ Unexpected error in CatchEvent:", error);
